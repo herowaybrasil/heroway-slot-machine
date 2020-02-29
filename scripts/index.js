6e2d1e620bc95109bf51b03a491a2a0101502704
Gtosta96 <gabrieltosta3@gmail.com>
2020-02-29 17:50:28 -0300

    reset to initial point

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+// valor disponível para aposta
+const availableToBet = 500;
+
+// itens para aposta
+const items = [
+  {
+    id: 1,
+    reward: 1000,
+    name: 'Batman',
+    src: './assets/img/batman.png',
+  }, {
+    id: 2,
+    reward: 120,
+    name: 'SpiderMan',
+    src: './assets/img/spiderman.png',
+  }, {
+    id: 3,
+    reward: -40,
+    name: 'Deadpool',
+    src: './assets/img/deadpool.png',
+  }, {
+    id: 4,
+    reward: -5000,
+    name: 'Bomb',
+    src: './assets/img/bomb.png',
+  },
+];
